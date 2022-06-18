db0eeb8674db920f3ad00cb8199e1ea5a8a88031
Alew <xalejandrow@gmail.com>
2022-06-18 00:00:19 +0000

    codigo conexion twitter

--- src/app.py
+++ src/app.py
@@ -1,12 +1,34 @@
 import os
+import request 
+import tweepy
+import pandas as pd 
 from dotenv import load_dotenv
+
 
 # load the .env file variables
 load_dotenv()
 
-#oyB4deatM5Kyf6JpA8Zvs4y2P
-#5qtvUmbw9LvRLqNlok17hMn5farHIIX1h12fUObxGG3YyDRga5
-#AAAAAAAAAAAAAAAAAAAAAKmGdwEAAAAAGFo7DR%2BLcB8bHJ78e%2BAGL1JrENk%3DWyBZ4HbluScPoCTN9BuOCv9d2vW4fqqLkam0C4cuQRsk3XDeZQ
+consumer_key = os.env.get("CONSUMER_KEY")
+consumer_secret = os.env.get("CONSUMER_SECRET")
+bearer_token= os.env.get("BEARER_TOKEN")
+
+# Creando cliente de Twitter
+
+client = tweepy.Client( bearer_token=bearer_token, 
+                        consumer_key=consumer_key, 
+                        consumer_secret=consumer_secret,
+                        return_type = requests.Response,
+                        wait_on_rate_limit=True)
+
+# Definiendo el query para Tweeter
+
+query = '#100daysofcode (pandas OR python) -is:retweet'      
+
+tweets = client.search_recent_tweets(query=query, 
+                                    tweet_fields=['author_id','created_at','lang'],
+                                     max_results=100)
+
+
 
 
 # your app code here
